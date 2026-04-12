c2c319102af6b5b0a6e0564e6248e73b7ebc6661
Luke Livingstone <info+github@rack.ac>
2026-04-12 18:22:37 +0100

    chore: Sledgehammer

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,10 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'subject-case': [
-      2,
-      'never',
-      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
-    ],
+    'subject-case': [0],
   },
 };
